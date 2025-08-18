597a10e645245b9f878acbe7a47b3422ece1d103
Duy Minh <minhphancailay@gmai.com>
2025-08-18 22:19:05 +0700

    update

--- frontend/src/components/CameraSection.jsx
+++ frontend/src/components/CameraSection.jsx
@@ -12,21 +12,29 @@ const CameraSection = ({
   loading,
   getChildCategories,
   showSubCategories,
-  filterByCategory,
+  setSelectedFilter,
 }) => {
-  console.log("Props: ", {
-    accessories,
-    category,
-    selectedFilter,
-    selectedCategoryId,
-  });
+  console.log("Props: ", accessories);
+  console.log("Category: ", category);
+  console.log("Selected Filter: ", selectedFilter);
+  console.log("Selected Category ID: ", selectedCategoryId);
 
   const toCapitalize = (str) => {
     return str ? str.charAt(0).toUpperCase() + str.slice(1).toLowerCase() : str;
   };
 
+  // const childCategories = getChildCategories(selectedFilter);
   const childCategories = getChildCategories();
-  console.log("Child Categories:", childCategories);
+  console.log(
+    "Child Categories for selectedFilter:",
+    selectedFilter,
+    "selectedCategoryId:",
+    selectedCategoryId,
+    "Result:",
+    childCategories
+  );
+
+  console.log("Show Sub Categories: ", showSubCategories);
 
   const [showSub, setShowSub] = useState(false);
 
@@ -59,9 +67,26 @@ const CameraSection = ({
                   <li
                     key={cat._id || cat.name}
                     className={`accessory-list-1 ${
-                      selectedFilter === cat.name.toLowerCase() ? "active" : ""
+                      selectedFilter === cat.name.toLowerCase()
+                        ? "accessory-list"
+                        : ""
                     }`}
-                    onClick={() => filterByCategory(cat.name)}
+                    onClick={() => {
+                      console.log(
+                        "Filtering by brand/category: ",
+                        cat.name.toLowerCase()
+                      );
+                      const filtered = accessories.filter((item) => {
+                        console.log("Item being filtered:", item); // Log từng item
+                        return (
+                          item.brand === cat.name.toLowerCase() ||
+                          (cat.name.toLowerCase() === "camera" &&
+                            item.category?.name?.toLowerCase() === "camera")
+                        );
+                      });
+                      setAccessories(filtered);
+                      setSelectedFilter(cat.name.toLowerCase());
+                    }}
                   >
                     <a href="#">{toCapitalize(cat.name)}</a>
                   </li>
@@ -80,62 +105,46 @@ const CameraSection = ({
               <div className="ajax-call">
                 <div className="list-product_camera">
                   <div className="owt-stage">
-                    {accessories.length > 0 ? (
-                      accessories.map((item, index) => (
-                        <div className="owt-item" key={item._id || index}>
-                          <li className="item__cate">
-                            <a href="#" className="item__cate-link">
-                              <div className="item-label"></div>
-                              <div className="item-img">
-                                <img
-                                  src={
-                                    item.image ||
-                                    "https://cdnv2.tgdd.vn/mwg-static/tgdd/Products/Images/4728/329241/camera-ip-360-do-3mp-ezviz-ty1-thumb-1-638665876017214332-600x600.jpg"
-                                  }
-                                  alt={item.name}
-                                  className="bounce"
-                                />
-                              </div>
-                              <p className="result-label">
-                                <img
-                                  src="https://cdnv2.tgdd.vn/mwg-static/common/Campaign/32/11/32110c30b7f564e80acd07741b81597d.png"
-                                  alt=""
-                                  width="20px"
-                                  height="20px"
-                                />
-                                <span>Sale Tựu Trường</span>
-                              </p>
-                              <h3 className="result-title">{item.name}</h3>
-                              <strong className="price">
-                                {item.finalPrice || item.price || "450.000"}₫
-                              </strong>
-                              <div className="box-p">
-                                <p className="price-old">
-                                  {item.price || "635.000"}₫
-                                </p>
-                                <span className="percent">
-                                  {item.discount?.discountValue || "-29"}%
-                                </span>
-                              </div>
-                            </a>
-                            <div className="item-bottom">
-                              <a href="#" className="shipping"></a>
-                            </div>
-                            <div className="rating_Compare">
-                              <div className="vote-txt">
-                                <b>{item.rating || "4.9"}</b>
-                              </div>
-                              <span>
-                                • Đã bán{" "}
-                                {item.soldQuantities?.[item._id] || "77,2k"}
-                              </span>
-                            </div>
-                          </li>
+                    <div className="owt-item">
+                      <li className="item__cate">
+                        <a href="#" className="item__cate-link">
+                          <div className="item-label"></div>
+                          <div className="item-img">
+                            <img
+                              src="https://cdnv2.tgdd.vn/mwg-static/tgdd/Products/Images/4728/329241/camera-ip-360-do-3mp-ezviz-ty1-thumb-1-638665876017214332-600x600.jpg"
+                              alt="Camera"
+                              className="bounce"
+                            />
+                          </div>
+                          <p className="result-label">
+                            <img
+                              src="https://cdnv2.tgdd.vn/mwg-static/common/Campaign/32/11/32110c30b7f564e80acd07741b81597d.png"
+                              alt=""
+                              width="20px"
+                              height="20px"
+                            />
+                            <span>Sale Tựu Trường</span>
+                          </p>
+                          <h3 className="result-title">
+                            Camera IP 360 Độ 3MP EZVIZ TY1
+                          </h3>
+                          <strong className="price">450.000₫</strong>
+                          <div className="box-p">
+                            <p className="price-old">635.000₫</p>
+                            <span className="percent">-29%</span>
+                          </div>
+                        </a>
+                        <div className="item-bottom">
+                          <a href="#" className="shipping"></a>
                         </div>
-                      ))
-                    ) : (
-                      <p>Không có sản phẩm để hiển thị</p>
-                    )}
+                        <div className="rating_Compare">
+                          <div className="vote-txt">
+                            <b>4.9</b>
+                          </div>
+                          <span>• Đã bán 77,2k</span>
+                        </div>
+                      </li>
+                    </div>
                   </div>
                 </div>
               </div>
